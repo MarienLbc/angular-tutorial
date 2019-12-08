2fef3a3691fe681b132356e263d94c515b6fb8d0
Marien Leblanc <marien.leblanc@amersports.com>
2019-12-08 15:44:08 +0100

    Tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { MatchDetailComponent } from './resources/matches/match-detail/match-detail.component';
+import { MatchListComponent } from './resources/matches/match-list/match-list.component';
+import { appRoutes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MatchDetailComponent,
+    MatchListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
